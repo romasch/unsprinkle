04f2f4102cf74ed87246dc900591abd40dd4fc2b
Roman Schmocker <roman.schmocker@ergon.ch>
2024-05-01 17:55:59 +0200

    Exercise 2 Image optimization

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -5,7 +5,11 @@ const PhotoGridItem = ({ id, src, alt, tags }) => {
   return (
     <article>
       <Anchor href={`/photos/${id}`}>
-        <Image src={src} />
+          <picture>
+              <source type={"image/avif"} srcSet={[src.replace(".jpg", ".avif 1x"), src.replace(".jpg", "@2x.avif 2x"), src.replace(".jpg", "@3x.avif 3x")].join(", ")}/>
+              <source type={"image/jpg"} srcSet={[src.replace(".jpg", ".jpg 1x"), src.replace(".jpg", "@2x.jpg 2x"), src.replace(".jpg", "@3x.jpg 3x")].join(", ")}/>
+              <Image src={src} />
+          </picture>
       </Anchor>
       <Tags>
         {tags.map((tag) => (
@@ -28,6 +32,7 @@ const Image = styled.img`
   height: 300px;
   border-radius: 2px;
   margin-bottom: 8px;
+  object-fit: cover;
 `;
 
 const Tags = styled.ul`
